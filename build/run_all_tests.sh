2de7a5a4d92e173b0aca1310d0f31c654a2486ac
gilmutdinov-ai <artur@gilmutdinov.ai>
2024-09-15 19:44:11 +0300

    dont regen ds by default

--- build/run_all_tests.sh
+++ build/run_all_tests.sh
@@ -1,14 +1,30 @@
 source "$(dirname "$0")/common.sh"
 
 # tests fail on old data
-rm data/ds/gened_visits_dataset.json 
+#rm data/ds/gened_visits_dataset.json 
 
 set -e
 
-if ! test -f data/ds/gened_visits_dataset.json; then
-  echo "data/ds/gened_visits_dataset.json not exist. Generating.."
+regen="false"
+fn="data/ds/gened_visits_dataset.json"
+
+while getopts 'r' flag; do
+  case "${flag}" in
+    r) regen=true;;
+  esac
+done
+
+if ! test -f $fn; then
+  echo "$fn not exist. Will regenerate.."
+  regen=true
+fi
+
+if [ "$regen" = true ] ; then
+  echo "$fn not exist. Generating.."
   cd infra/local/kafka/ && npm install . ; cd -
   node infra/local/kafka/gen_visits_ds.js file
+else
+  echo "Will use existing $fn (-r to regenrate or delete it)"
 fi
 
 #CC=clang bazel test --experimental_enable_bzlmod --cxxopt=-std=c++20 --copt="-g" --copt -fsanitize=address --copt -DADDRESS_SANITIZER --linkopt -fsanitize=address --strip=never //src/tests
