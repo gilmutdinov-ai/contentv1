6f5d6a43f32a09d075313cf4f643503c3f6b1353
gilmutdinov-ai <artur@gilmutdinov.ai>
2024-09-19 12:40:43 +0300

    fix flag

--- build_all.sh
+++ build_all.sh
@@ -6,7 +6,7 @@ source "$(dirname "$0")/build/common.sh"
 # run in dev container build/run_shell_docker.sh
 #  built with build/docker_build_dev_image.sh
 
-build/run_all_tests.sh
+build/run_all_tests.sh -r
 build/build_contentd.sh
 build/build_kafka_loader.sh
 build/build_sched.sh
